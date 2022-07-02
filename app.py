2c15a38db5e563b63538c8302c21d52660651d74
Ayumi <ayuayuayuayus@gmail.com>
2022-07-02 21:33:40 +0200

    test2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 from flask import Flask, request, abort, jsonify, render_template, url_for, flash, redirect
 from flask_cors import CORS
 # test comment
+# test comment
 
 import traceback
 from forms import NewLocationForm
