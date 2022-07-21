34dd0aa37b6a66e4e2a6970ebe0c2ec0499a49c2
Ayumi <ayuayuayuayus@gmail.com>
2022-07-21 10:08:12 +0200

    multiselect succeeded

--- forms.py
+++ forms.py
@@ -7,6 +7,15 @@ from wtforms.validators import DataRequired, Length
 from pycountry import languages, pycountry
 from django import forms
 
+class MultiCheckboxField(SelectMultipleField):
+    """
+    A multiple-select, except displays a list of checkboxes.
+
+    Iterating the field will produce subfields, allowing custom rendering of
+    the enclosed checkbox fields.
+    """
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class NewLocationForm(FlaskForm):
     description = StringField('Location description',
@@ -28,6 +37,10 @@ class NewLocationForm(FlaskForm):
     # LANGUAGE_CHOICES = [(language.name, language.name) for language in pycountry.languages]
     # choices = ['apple', 'banana','cherry']
     # language_speak = SelectField('Which programming Language do you speak?', validators=[DataRequired()], choices=choices)
+    options=['12 available options:', 'French', 'Spanish', 'English', 'Portuguese', 'Chinese', 'German','Khoisan', 'Korean', 'Swahili', 'Japanese', 'Russian', 'Arabic']
+    # options.sort()
+
+    # language_speak = SelectMultipleField('Your native and fluent language(s)', validators=[DataRequired()], choices=options)
 
     # class MultiCheckboxField(SelectMultipleField):
     #     widget = widgets.ListWidget(prefix_label=False)
@@ -36,18 +49,15 @@ class NewLocationForm(FlaskForm):
     # class ExampleForm(FlaskForm):
     # language_speak = SelectMultipleField('label',coerce=int,choices=[(1, 'one'), (2, 'two'), (3, 'three')],validators=[])
     # widget = widgets.CheckboxInput()
-    # print(widget)
-    # language_speak = BooleanField('title',             default=True,              render_kw ={'checked':''})
-
-    language_speak = forms.MultipleChoiceField(choices=(
-    (1, 'Ibaraki Moriya-city'),
-    (2, 'Saitama Sakado-city'),
-    (3, 'Aichi Inazawa-city'),
-    (4, 'Osaka Takatsuki-city'),
-  ), widget=forms.CheckboxSelectMultiple, label='メーカー工場')
+    # # print(widget)
+    # language_speak = BooleanField('title',             false_values=None,              render_kw ={'checked':''})
+    language_speak = MultiCheckboxField('Routes', choices = options, coerce=int)
+    # language_speak = forms.DecimalField(initial=300, max_value=1000)
 
 # render_kw={"placeholder": "Select language"}
     submit = SubmitField('Create Location')
+
+
 
     # print(list(pycountry.languages))
     # print(list(pycountry.languages.common_name))
